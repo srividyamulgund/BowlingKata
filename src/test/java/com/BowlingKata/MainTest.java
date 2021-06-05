7a4d601d131efc2401f4c99c35b316908f966378
Srividya Mulgund <srividya.mulgund@gmail.com>
2021-06-05 21:17:45 +0100

    Added additional test case for more test coverage.

--- src/test/java/com/BowlingKata/MainTest.java
+++ src/test/java/com/BowlingKata/MainTest.java
@@ -38,8 +38,10 @@ class MainTest {
     @Test
     @DisplayName("Returns calculated score when some spare and strike")
     public void testFinalScoreWithVariedInputs() {
-        String inputString = "X 7/ 72 9/ X X X 23 6/ 7/3";
-        assertEquals(168, Main.getFinalScore(inputString), "Calculation Mismatch");
+        String inputString1 = "X 7/ 72 9/ X X X 23 6/ 7/3";
+        assertEquals(168, Main.getFinalScore(inputString1), "Calculation Mismatch");
+        String inputString2 = "X 9/ 5/ 72 X X X 9- 8/ 9/X";
+        assertEquals(187, Main.getFinalScore(inputString2), "Calculation Mismatch");
     }
 
 }
